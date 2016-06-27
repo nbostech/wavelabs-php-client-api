36b75f433df4c25fe61c02832706ba16cfa13203
L B Sastry <lbsastry@nibos.com>
2016-06-27 17:02:37 +0530

    Config file added

---
+++ src/NBOS/config.php
@@ -0,0 +1,6 @@
+<?php
+    defined('API_HOST_URL')         OR define('API_HOST_URL', "http://api.qa1.nbos.in/");
+
+    defined('API_CLIENT_ID')        OR define('API_CLIENT_ID', "appConsole-app-client");
+    defined('API_CLIENT_SECRET')    OR define('API_CLIENT_SECRET', "appConsole-app-secret");
+?>
